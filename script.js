7f1ba4077bdef62825942798a63e45fcd2dde899
N0r4nd0 <normandomagalhaes@gmail.com>
2022-04-20 10:34:45 -0300

    ultima atualização

--- script.js
+++ script.js
@@ -14,6 +14,17 @@ function ghibli(){
 function loginTab(){
     window.location.href = "login.html";
 }
+// Codigo para mudar imagem de fundo da tela de login 
+
+var random = Math.random() * (4 - 1) + 1;
+if (random < 2){
+    document.getElementById("image").style.backgroundImage = "url(https://www.planocritico.com/wp-content/uploads/2021/06/studio_ghibli-plano-critico-filme-animacao-anime.jpg)";
+}else if(random >= 3){
+    document.getElementById("image").style.backgroundImage = "url(https://hbomax-images.warnermediacdn.com/images/GXnEEoQ4sRMNVoAEAAB0c/tileburnedin?size=1280x720&partner=hbomaxcom&v=0b38e80a27d36536919361793b6f744b&language=pt-br&host=art-gallery.api.hbo.com&w=Infinity";
+}else{
+    document.getElementById("image").style.backgroundImage = "url(https://observatoriodocinema.uol.com.br/wp-content/uploads/2021/04/MCU-divulgacao.jpg)";
+}
+
 // Verificações de formularios
 const form = document.getElementById('form');
 const username = document.getElementById('username');
@@ -85,4 +96,3 @@ function setSuccessFor(input) {
 function isEmail(email) {
     return /^(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/.test(email);
 }
-// Codigo para mudar imagem de fundo da tela de login 
